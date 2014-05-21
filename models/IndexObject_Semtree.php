278da100e33ff2844a7f82d9cd6184de00186ebb
Florian Bieringer <florian.bieringer@uni-passau.de>
2014-05-21 09:09:23 +0200

    SemTree Indexing

---
+++ models/IndexObject_Semtree.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Description of IndexObject_Seminar
+ *
+ * @author intelec
+ */
+class IndexObject_Semtree {
+
+    const RATING_SEMTREE = 1.0;
+
+    public static function sqlIndex() {
+        IndexManager::createObjects("SELECT sem_tree_id, 'semtree', name, CONCAT('sem_portal.php?start_item_id',sem_tree_id) FROM sem_tree");
+        IndexManager::createIndex("SELECT object_id, name, " . self::RATING_SEMTREE . " FROM sem_tree " . IndexManager::createJoin('sem_tree_id'));
+    }
+
+}
