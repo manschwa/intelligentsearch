ca24ccf6e70cd49880e6746869a0b0216cda8cfb
Florian Bieringer <florian.bieringer@uni-passau.de>
2014-05-21 09:31:21 +0200

    Altcode tot

--- controllers/show.php
+++ controllers/show.php
@@ -27,29 +27,6 @@ class ShowController extends StudipController {
         $this->time = IndexManager::sqlIndex();
     }
 
-    public function create_action($type = null) {
-        if ($type) {
-            $this->msg = IndexManager::index($type);
-        } else {
-            $this->msg = "Update finished after: ".IndexManager::indexAll()." Seconds";
-        }
-    }
-    
-    public function update_action() {
-        if (IndexManager::update()) {
-            $this->msg = ("Exhausted! Run again please!");
-        } else {
-            $this->msg = ("Update done!");
-        }
-        $this->render_action('create');
-    }
-    
-    public function reset_action() {
-        DBManager::get()->query('TRUNCATE TABLE search_index');
-        DBManager::get()->query('TRUNCATE TABLE search_object');
-        $this->render_action('create');
-    }
-
     // customized #url_for for plugins
     function url_for($to) {
         $args = func_get_args();
