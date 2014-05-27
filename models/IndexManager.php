4f80faad1a06de9d2aa5454d1c6171fce09bb1b9
Florian Bieringer <florian.bieringer@uni-passau.de>
2014-05-27 14:31:44 +0200

    Non block reindexing

--- models/IndexManager.php
+++ models/IndexManager.php
@@ -11,23 +11,24 @@ class IndexManager {
 
     public static function sqlIndex($restriction = null) {
         set_time_limit(3600);
+        $db = DBManager::get();
         $time = time();
-        if ($restriction) {
-            DBManager::get()->execute('DELETE search_object,search_index FROM search_object JOIN search_index USING (object_id) WHERE type LIKE ?', array($restriction));
-        } else {
-            DBManager::get()->query('TRUNCATE TABLE search_object');
-            DBManager::get()->query('TRUNCATE TABLE search_index');
-        }
+        $db->query('CREATE TABLE search_object_temp LIKE search_object');
+        $db->query('CREATE TABLE search_index_temp LIKE search_index');
         foreach (glob(__DIR__ . '/IndexObject_*') as $indexFile) {
             $type = explode('_', $indexFile);
             if (!$restriction || stripos(array_pop($type), $restriction) !== false) {
                 $indexClass = basename($indexFile, ".php");
-                DBManager::get()->query("ALTER TABLE search_index DISABLE KEYS");
+                $db->query("ALTER TABLE search_index DISABLE KEYS");
                 $indexClass::sqlIndex();
-                DBManager::get()->query("ALTER TABLE search_index ENABLE KEYS");
-                DBManager::get()->query("OPTIMIZE TABLE search_index");
+                $db->query("ALTER TABLE search_index ENABLE KEYS");
+                $db->query("OPTIMIZE TABLE search_index");
             }
         }
+        $db->query('DROP TABLE search_object');
+        $db->query('RENAME TABLE search_object_temp TO search_object');
+        $db->query('DROP TABLE search_index');
+        $db->query('RENAME TABLE search_index_temp TO search_index');
         return time() - $time;
     }
 
@@ -38,11 +39,11 @@ class IndexManager {
      * @param SQL SQL for the input
      */
     public static function createObjects($sql) {
-        DBManager::get()->query("INSERT INTO search_object (range_id, type, title, range2, range3) ($sql)");
+        DBManager::get()->query("INSERT INTO search_object_temp (range_id, type, title, range2, range3) ($sql)");
     }
 
     public static function createIndex($sql) {
-        DBManager::get()->query("INSERT INTO search_index (object_id, text, relevance) ($sql)");
+        DBManager::get()->query("INSERT INTO search_index_temp (object_id, text, relevance) ($sql)");
     }
 
     public static function relevance($base, $modifier) {
