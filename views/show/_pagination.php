93b48a3c90aa6bc4ba85abd93f16092a4672e937
Florian Bieringer <florian.bieringer@uni-passau.de>
2014-06-03 10:56:05 +0200

    No search vs pagination fix

--- views/show/_pagination.php
+++ views/show/_pagination.php
@@ -1,4 +1,4 @@
-<? if ($search->countResultPages() > 1): ?>
+<? if ($search && $search->countResultPages() > 1): ?>
     <div class='pagination'>
         <? foreach ($search->getPages(Request::get('page')) as $page): ?>
             <a href='<?= URLHelper::getLink('', array('search' => $search->query, 'filter' => $search->filter, 'page' => $page)) ?>' class='<?= Request::get('page') == $page ? 'current' : ''?>'>
