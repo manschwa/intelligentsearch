ea7790e7c6ff0cd8fe4c63e83064b28f7b50b56b
Florian Bieringer <florian.bieringer@uni-passau.de>
2014-06-12 15:09:56 +0200

    Fix useless if

--- controllers/show.php
+++ controllers/show.php
@@ -32,7 +32,7 @@ class ShowController extends StudipController {
     }
 
     public function fast_action($restriction = null) {
-        if ($GLOBALS['perm']->check('root'));
+        $GLOBALS['perm']->check('root');
         $this->time = IndexManager::sqlIndex($restriction);
         $this->redirect('show/index');
     }
