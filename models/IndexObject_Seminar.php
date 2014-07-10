b1514e79fc99916ccdeb94d33230ba7431b7001c
Florian Bieringer <florian.bieringer@uni-passau.de>
2014-07-10 09:24:04 +0200

    Do the indexing that fails the most at last

--- models/IndexObject_Seminar.php
+++ models/IndexObject_Seminar.php
@@ -10,15 +10,15 @@ class IndexObject_Seminar {
     public static function sqlIndex() {
         IndexManager::createObjects("SELECT seminar_id, 'seminar', CONCAT_WS(' ', sd.name, s.Veranstaltungsnummer, s.name), null,null FROM seminare s JOIN semester_data sd ON s.start_time BETWEEN sd.beginn AND sd.ende");
         IndexManager::createIndex("SELECT object_id, CONCAT_WS(' ', Veranstaltungsnummer, Name), " . IndexManager::relevance(self::RATING_SEMINAR, 'start_time') . " FROM seminare JOIN search_object ON (seminar_id = range_id)");
+        IndexManager::createIndex("SELECT object_id, Untertitel, " . IndexManager::relevance(self::RATING_SEMINAR_SUBTITLE, 'start_time') . " FROM seminare JOIN search_object ON (seminar_id = range_id) WHERE Untertitel != ''");
+        IndexManager::createIndex("SELECT object_id, Beschreibung, " . IndexManager::relevance(self::RATING_SEMINAR_OTHER, 'start_time') . " FROM seminare JOIN search_object ON (seminar_id = range_id) WHERE Beschreibung != ''");
+        IndexManager::createIndex("SELECT object_id, Sonstiges, " . IndexManager::relevance(self::RATING_SEMINAR_OTHER, 'start_time') . " FROM seminare JOIN search_object ON (seminar_id = range_id) WHERE Sonstiges != ''");
         IndexManager::createIndex("SELECT object_id, CONCAT_WS(' ',i.title_front, a.Vorname, a.Nachname), " . IndexManager::relevance(self::RATING_SEMINAR_DOZENT, 'start_time') . "
 FROM seminare s 
 JOIN search_object ON (s.seminar_id = range_id) 
 JOIN seminar_user u ON (s.seminar_id = u.seminar_id AND u.status = 'dozent')
 JOIN auth_user_md5 a ON (u.user_id = a.user_id)
 JOIN user_info i ON (u.user_id = i.user_id)");
-        IndexManager::createIndex("SELECT object_id, Untertitel, " . IndexManager::relevance(self::RATING_SEMINAR_SUBTITLE, 'start_time') . " FROM seminare JOIN search_object ON (seminar_id = range_id) WHERE Untertitel != ''");
-        IndexManager::createIndex("SELECT object_id, Beschreibung, " . IndexManager::relevance(self::RATING_SEMINAR_OTHER, 'start_time') . " FROM seminare JOIN search_object ON (seminar_id = range_id) WHERE Beschreibung != ''");
-        IndexManager::createIndex("SELECT object_id, Sonstiges, " . IndexManager::relevance(self::RATING_SEMINAR_OTHER, 'start_time') . " FROM seminare JOIN search_object ON (seminar_id = range_id) WHERE Sonstiges != ''");
     }
 
     public static function getCondition() {
