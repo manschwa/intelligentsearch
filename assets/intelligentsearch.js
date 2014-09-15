d01346d59abf42a99e6916e36916a3dab8d436f0
Florian Bieringer <florian.bieringer@uni-passau.de>
2014-09-15 11:16:14 +0200

    Fix printpreview

--- assets/intelligentsearch.js
+++ assets/intelligentsearch.js
@@ -1,17 +1,21 @@
 $(document).ready(function() {
 
-    // Remove old quicksearch
-    $('#quicksearch').children().remove();
+    var quicksearch = $('#quicksearch');
 
-    // Add new quicksearch
-    $('#quicksearch').prepend($('.intelligentsearch').keypress(function(event) {
-        var keycode = (event.keyCode ? event.keyCode : event.which);
-        if (keycode == '13') {
-            event.preventDefault();
-            var params = {'search': $('.intelligentsearch').val(),
-                'utf8': true
-            };
-            window.location = STUDIP.URLHelper.getURL('plugins.php/intelligentesucheplugin/show/index', params);
-        }
-    }).show());
+    if (quicksearch.length) {
+        // Remove old quicksearch
+        $('#quicksearch').children().remove();
+
+        // Add new quicksearch
+        $('#quicksearch').prepend($('.intelligentsearch').keypress(function(event) {
+            var keycode = (event.keyCode ? event.keyCode : event.which);
+            if (keycode == '13') {
+                event.preventDefault();
+                var params = {'search': $('.intelligentsearch').val(),
+                    'utf8': true
+                };
+                window.location = STUDIP.URLHelper.getURL('plugins.php/intelligentesucheplugin/show/index', params);
+            }
+        }).show());
+    }
 });
