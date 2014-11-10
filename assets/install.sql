5f2121f3cba2bd299b0e5c6a14aa75336507eea8
Florian Bieringer <florian.bieringer@uni-passau.de>
2014-11-10 10:43:39 +0100

    Fix COLLATE information

--- assets/install.sql
+++ assets/install.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `search_index` (
   `object_id` int(11) NOT NULL,
-  `text` text COLLATE latin1_german1_ci NOT NULL,
+  `text` text NOT NULL,
   `relevance` float NOT NULL,
   KEY `object_id` (`object_id`),
   KEY `relevance` (`relevance`),
@@ -9,11 +9,11 @@ CREATE TABLE `search_index` (
 
 CREATE TABLE `search_object` (
   `object_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `range_id` varchar(32) COLLATE latin1_german1_ci NOT NULL DEFAULT '',
-  `type` varchar(255) COLLATE latin1_german1_ci NOT NULL DEFAULT '',
-  `title` varchar(255) COLLATE latin1_german1_ci NOT NULL DEFAULT '',
-  `range2` varchar(32) COLLATE latin1_german1_ci DEFAULT NULL,
-  `range3` varchar(32) COLLATE latin1_german1_ci DEFAULT NULL,
+  `range_id` varchar(32) NOT NULL DEFAULT '',
+  `type` varchar(255) NOT NULL DEFAULT '',
+  `title` varchar(255) NOT NULL DEFAULT '',
+  `range2` varchar(32) DEFAULT NULL,
+  `range3` varchar(32) DEFAULT NULL,
   PRIMARY KEY (`object_id`),
   KEY `range_id` (`range_id`),
   KEY `type` (`type`)
